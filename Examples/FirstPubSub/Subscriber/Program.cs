a26b20d77d7cf212e9724ecf4ceb381d0a12d1e4
Rob White <rob.j.white@gmail.com>
2018-05-17 17:37:16 +0100

    Hello World Example

---
+++ Examples/FirstPubSub/Subscriber/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+using Microsoft.Extensions.DependencyInjection;
+using Pat.Subscriber;
+using Pat.Subscriber.NetCoreDependencyResolution;
+using Pat.Subscriber.Telemetry.StatsD;
+
+namespace Subscriber
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            var serviceProvider = InitialiseIoC();
+
+            var tokenSource = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, args) =>
+            {
+                var log = serviceProvider.GetService<ILog>();
+                log.Info("Subscriber Shutdown Requested");
+                args.Cancel = true;
+                tokenSource.Cancel();
+            };
+
+            var subscriber = serviceProvider.GetService<Pat.Subscriber.Subscriber>();
+            await subscriber.Initialise(new[] {Assembly.GetExecutingAssembly()});
+            await subscriber.ListenForMessages(tokenSource);
+        }
+
+        private static ServiceProvider InitialiseIoC()
+        {
+            var connection = "Endpoint=sb://namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=YOURKEY";
+            var topicName = "pat";
+
+            var subscriberConfiguration = new SubscriberConfiguration
+            {
+                ConnectionStrings = new[] {connection},
+                TopicName = topicName,
+                SubscriberName = "PatExampleSubscriber",
+                UseDevelopmentTopic = false
+            };
+
+            InitLogger();
+
+            var serviceProvider = new ServiceCollection()
+                .AddPatLite(subscriberConfiguration)
+                .AddTransient(s => LogManager.GetLogger(s.GetType()))
+                .AddTransient<IStatisticsReporter, StatisticsReporter>()
+                .AddSingleton(new StatisticsReporterConfiguration())
+                .AddHandlersFromAssemblyContainingType<FooHandler>()
+                .BuildServiceProvider();
+
+            return serviceProvider;
+        }
+
+        private static void InitLogger()
+        {
+            var hierarchy = (Hierarchy)LogManager.GetRepository(Assembly.GetExecutingAssembly());
+            var tracer = new TraceAppender();
+            var patternLayout = new PatternLayout();
+
+            patternLayout.ConversionPattern = "%d [%t] %-5p %m%n";
+            patternLayout.ActivateOptions();
+
+            tracer.Layout = patternLayout;
+            tracer.ActivateOptions();
+            hierarchy.Root.AddAppender(tracer);
+
+            var appender = new ConsoleAppender();
+            appender.Layout = patternLayout;
+            appender.ActivateOptions();
+            hierarchy.Root.AddAppender(appender);
+
+            hierarchy.Root.Level = Level.All;
+            hierarchy.Configured = true;
+        }
+    }
+}
