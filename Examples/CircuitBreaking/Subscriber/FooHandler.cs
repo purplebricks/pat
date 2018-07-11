cc39e95bd3b2e06717a1c839da110abd85c0f018
Rob White <rob.j.white@gmail.com>
2018-07-11 15:16:08 +0100

    Add Circuit Breaker Example

---
+++ Examples/CircuitBreaking/Subscriber/FooHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Contract;
+using log4net;
+using Pat.Subscriber;
+
+namespace Subscriber
+{
+    public class FooHandler : IHandleEvent<Foo>
+    {
+        private readonly ILog _log;
+
+        public FooHandler(ILog log)
+        {
+            _log = log;
+        }
+
+        public Task HandleAsync(Foo @event)
+        {
+            _log.Info($"Handling: {@event}");
+            if (DateTime.UtcNow.Minute % 5 == 0)
+            {
+                throw new ExternalProviderOfflineException();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
