eb1c0cf4f1f552234e74449f74bf5bb3bfe896fe
Dave Thompson <david@aurionix.com>
2018-07-21 09:38:37 +0100

    Update Publishers to .NET Core Logging
    Except for HelloWorldNetFramework to demonstrate connecting log4net with StructureMap.

--- Examples/NonTransientMessageFailureHandling/Publisher/Program.cs
+++ Examples/NonTransientMessageFailureHandling/Publisher/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Reflection;
 using System.Threading.Tasks;
 using Contract;
-using log4net;
-using log4net.Appender;
-using log4net.Core;
-using log4net.Layout;
-using log4net.Repository.Hierarchy;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Pat.Sender;
 using Pat.Sender.Correlation;
 using Pat.Sender.MessageGeneration;
+using Pat.Sender.NetCoreLog;
 
 namespace Publisher
 {
@@ -36,47 +32,33 @@ namespace Publisher
             var connection = "Endpoint=sb://namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=YOURKEY";
             var topicName = "pat";
 
-            var sender = new PatSenderSettings
+            var settings = new PatSenderSettings
             {
                 TopicName = topicName,
                 PrimaryConnection = connection,
                 UseDevelopmentTopic = false
             };
 
-            InitLogger();
-
             var serviceProvider = new ServiceCollection()
-                .AddSingleton(sender)
-                .AddTransient<IMessagePublisher, MessagePublisher>()
-                .AddTransient<IMessageSender, MessageSender>()
-                .AddTransient(s => LogManager.GetLogger(s.GetType()))
-                .AddTransient<IMessageGenerator, MessageGenerator>()
-                .AddTransient(s => new MessageProperties(new LiteralCorrelationIdProvider($"{Guid.NewGuid()}")))
-                .BuildServiceProvider();
+                 .AddPatSender(settings)
+                 .AddNetCoreLogging()
+                 .BuildServiceProvider();
 
             return serviceProvider;
         }
 
-        private static void InitLogger()
-        {
-            var hierarchy = (Hierarchy)LogManager.GetRepository(Assembly.GetExecutingAssembly());
-            var tracer = new TraceAppender();
-            var patternLayout = new PatternLayout();
+        private static IServiceCollection AddPatSender(this IServiceCollection services, PatSenderSettings settings)
+            => services
+                .AddSingleton(settings)
+                .AddTransient<IMessagePublisher, MessagePublisher>()
+                .AddTransient<IMessageSender, MessageSender>()
+                .AddTransient<IMessageGenerator, MessageGenerator>()
+                .AddTransient(s => new MessageProperties(new LiteralCorrelationIdProvider($"{Guid.NewGuid()}")));
 
-            patternLayout.ConversionPattern = "%d [%t] %-5p %m%n";
-            patternLayout.ActivateOptions();
-
-            tracer.Layout = patternLayout;
-            tracer.ActivateOptions();
-            hierarchy.Root.AddAppender(tracer);
-
-            var appender = new ConsoleAppender();
-            appender.Layout = patternLayout;
-            appender.ActivateOptions();
-            hierarchy.Root.AddAppender(appender);
-
-            hierarchy.Root.Level = Level.All;
-            hierarchy.Configured = true;
-        }
+        private static IServiceCollection AddNetCoreLogging(this IServiceCollection services)
+            => services
+                .AddTransient<IPatSenderLog, PatSenderNetCoreLogAdapter>()
+                .AddLogging(b => b.AddConsole())
+                .AddSingleton(s => s.GetRequiredService<ILoggerFactory>().CreateLogger("Publisher"));
     }
 }
